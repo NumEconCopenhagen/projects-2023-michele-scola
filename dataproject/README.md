51f3b84e47aa769516107add7e413e0ce6a7232b
Michelescola <whr900@alumni.ku.dk>
2023-04-16 20:42:49 +0200

    final

--- dataproject/README.md
+++ dataproject/README.md
@@ -1,14 +1,12 @@
 # Data analysis project
 
-Our project is titled **PROJECT TITLE** and is about EXPLAIN.
+My project is titled **COVID DEATHS AND HOSPITAL BEDS** and is about correlation between covid-19 deaths and number of hospital beds by country.
 
 The **results** of the project can be seen from running [dataproject.ipynb](dataproject.ipynb).
+Funcions can be seen in the [dataproject.py](dataproject.py) file
 
-We apply the **following datasets**:
+I have used the **following datasets**:
 
-1. dataX.csv (*source*) 
-1. dataY.csv (*source*)
-
-**Dependencies:** Apart from a standard Anaconda Python 3 installation, the project requires the following installations:
-
-``pip install matplotlib-venn``
+1. data.csv (*https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv*) 
+2. (*https://data.worldbank.org/indicator/SH.MED.BEDS.ZS*)
+3. (*https://data.worldbank.org/indicator/EN.POP.DNST*)
--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,16 +1,63 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
+import matplotlib.pyplot as plt
+import numpy as np
 
-    Args:
-        df (pd.DataFrame): pandas dataframe 
+def plot(cov, country): 
+    I = cov['country'] == country
+    ax=cov.loc[I,:].plot(x='day', y='death', style='-', legend=False)
+    ax.set_title('covid-19 death by country')
+    ax.set_ylabel('death');
 
-    Returns:
-        df (pd.DataFrame): pandas dataframe
+def plot2(bed, country): 
+    I = bed['country'] == country
+    ax=bed.loc[I,:].plot(x='year', y='beds', style='-o', legend=False)
+    plt.axhline(y=np.mean(bed.loc[I,'beds']), color='r', label='mean')
+    ax.set_title('beds per 1000 people by country')
+    ax.set_ylabel('beds/1000 people')
+    ax.legend(loc='upper left', prop={'size': 10});
 
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
+def plot3(den, country): 
+    I = den['country'] == country
+    ax=den.loc[I,:].plot(x='year', y='density', style='-o', legend=False)
+    plt.axhline(y=np.mean(den.loc[I,'density']), color='r', label='mean')
+    ax.set_title('density (people per sq.km) by country')
+    ax.set_ylabel('density')
+    ax.legend(loc='upper left', prop={'size': 10});
+
+def plot4(inner2, country):
+    I = inner2['country'] == country
+    ax = inner2.loc[I,:].plot(x='year', y='density', style='-o', legend=False)
+    plt.axhline(y=np.mean(inner2.loc[I,'density']), color='r', label='mean')
+    ax.set_title('density (people per sq.km) by country')
+    ax.set_ylabel('density')
+    ax.legend(loc='upper left', prop={'size': 10});
+    ax = inner2.loc[I,:].plot(x='year', y='beds', style='-o', legend=False)
+    plt.axhline(y=np.mean(inner2.loc[I,'beds']), color='r', label='mean')
+    ax.set_title('beds by country')
+    ax.set_ylabel('beds per 1000 people')
+    ax.legend(loc='upper left', prop={'size': 10});
+
+def red_country(inner, country):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    I = inner['country'] == country
+    ax.scatter(inner.loc[:,'mean'],inner.iloc[:,1143])
+    ax.scatter(inner.loc[I,'mean'],inner.loc[I,'tot_death'], color='r')
+    a, b = np.polyfit(inner.loc[:,'mean'],inner.iloc[:,1143], 1)
+    ax.plot(inner.loc[:,'mean'], a*(inner.loc[:,'mean'])+b, color='y', linestyle='-.', label='fitting plot line')
+    ax.set_title('covid-19 deaths against hospital beds')
+    ax.set_xlabel('hospital beds per 1000 people')
+    ax.set_ylabel('covid-19 deaths')
+    ax.legend(loc='upper right', prop={'size': 10});
+
+def red_country2(inner, country):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    I = inner['country'] == country
+    ax.scatter(inner.loc[:,'mean'],inner.loc[:,'last'])
+    ax.scatter(inner.loc[I,'mean'],inner.loc[I,'last'], color = 'r')
+    a, b = np.polyfit(inner.loc[:,'mean'],inner.loc[:,'last'], 1)
+    ax.plot(inner.loc[:,'mean'], a*(inner.loc[:,'mean'])+b, color='y', linestyle='-.', label='fitting plot line')
+    ax.set_title('density in 2020 against hospital beds')
+    ax.set_xlabel('hospital beds per 1000 people')
+    ax.set_ylabel('density')
+    ax.legend(loc='upper right', prop={'size': 10});
